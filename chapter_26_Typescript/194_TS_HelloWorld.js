5c953999df130679f145fc320d8e67a7ae4af5ab
PradnyaTilekar <Pradnya.tilekar9@gmail.com>
2026-06-28 18:46:38 +0530

    chore: add new files

---
+++ chapter_26_Typescript/194_TS_HelloWorld.js
@@ -0,0 +1,7 @@
+"use strict";
+let testName = "Login Test";
+function add_ts(a, b) {
+    return a + b;
+}
+let sum = add_ts(12, 45);
+console.log(sum);
---
+++ chapter_26_Typescript/194_TS_HelloWorld.ts
@@ -0,0 +1,8 @@
+let testName: string = "Login Test";
+
+function add_ts(a: number, b: number): number {
+    return a + b
+}
+let sum = add_ts(12,45);
+console.log(sum );
+
